a52041b0de579c907eb0f26a9dc4729378fb914e
Aiysosis <aiysosis@qq.com>
2022-12-18 16:54:11 +0800

    ignore me

--- src/render.ts
+++ src/render.ts
@@ -12,6 +12,8 @@ export type Fiber = {
 	parent: Fiber;
 	sibling: Fiber;
 	child: Fiber;
+	alternate: Fiber; //指向老的 fiber节点
+	effectTag: string;
 };
 
 //* 全局变量：下一个要执行的 Fiber
@@ -19,6 +21,10 @@ let nextUnitOfWork: Fiber = null;
 //* 全局变量 witRoot, 暂存没有渲染完的 根fiber,等全部渲染完成之后把整棵树一起挂到 root container上
 //* 这样用户就不会看到只渲染了一部分的界面
 let witRoot: Fiber = null;
+//* 记录当前渲染的 fiber树，用于更新
+let currentRoot: Fiber = null;
+//* 记录需要删除的fiber，用于统一删除
+let deletions: Fiber[] = null;
 
 export function render(element: ElementNode, container: HTMLElement | Text) {
 	//* 设置根部的 Fiber,它唯一的孩子节点是 element，父fiber和兄弟fiber都是空
@@ -31,7 +37,10 @@ export function render(element: ElementNode, container: HTMLElement | Text) {
 		parent: null,
 		sibling: null,
 		child: null,
+		alternate: currentRoot,
+		effectTag: "",
 	};
+	deletions = [];
 	nextUnitOfWork = witRoot = rootFiber;
 }
 
@@ -51,15 +60,24 @@ function workLoop(deadline: IdleDeadline) {
 	requestIdleCallback(workLoop);
 }
 
+//? 之前没有接触过的 api，它会在浏览器有空闲的时候执行我们的Fiber，并获取下一个 Fiber，直到执行完毕
+//? 可以理解为 setTimeout,只是这个延迟执行是由浏览器自动调度的，我们通过requestIdleCallback 注册回调之后
+//? 浏览器会在空闲的时候响应我们注册的请求
+
+requestIdleCallback(workLoop);
+
 function commitRoot() {
 	//* 此时所有fiber内部的dom都已创建完成
+	deletions.forEach(fiber => commitWork(fiber));
 	commitWork(witRoot.child);
+	currentRoot = witRoot; //备份
 	witRoot = null;
 }
 
+//* 挂载和删除都在这里
 function commitWork(fiber: Fiber) {
 	//最开始拿到的是根节点的 child，也就是最外层元素对应的fiber
-	//? 从上往下挂载 or 从下往上挂载？
+	//? 从上往下挂载 or 从下往上挂载？ 前者，递归
 	if (!fiber) return;
 	const parentDom = fiber.parent.dom;
 	parentDom.appendChild(fiber.dom);
@@ -67,45 +85,17 @@ function commitWork(fiber: Fiber) {
 	commitWork(fiber.sibling);
 }
 
-//? 之前没有接触过的 api，它会在浏览器有空闲的时候执行我们的Fiber，并获取下一个 Fiber，直到执行完毕
-//? 可以理解为 setTimeout,只是这个延迟执行是由浏览器自动调度的，我们通过requestIdleCallback 注册回调之后
-//? 浏览器会在空闲的时候响应我们注册的请求
-
-requestIdleCallback(workLoop);
-
 function performUnitOfWork(fiber: Fiber): Fiber {
 	//* add dom node
 	if (!fiber.dom) {
 		fiber.dom = createDom(fiber);
 	}
-	//* 向父元素添加dom元素
-	//! 不直接提交，而是等全部节点创建完毕后统一提交 @ref -> witRoot
-	// if (fiber.parent) {
-	// 	fiber.parent.dom.appendChild(fiber.dom);
-	// }
 
 	//* create new fibers
 	const children = fiber.props.children;
-	let prevSibling: Fiber = null;
+	//? diff 算法
+	reconcileChildren(children, fiber);
 
-	for (let i = 0; i < children.length; i++) {
-		const child = children[i];
-		const newFiber: Fiber = {
-			type: child.type,
-			props: child.props,
-			dom: null, // to be created when perform
-			parent: fiber,
-			sibling: null, //to be set immediately
-			child: null, //to be set when perform
-		};
-		//* 将 newFiber找个地方存起来
-		if (i === 0) {
-			fiber.child = newFiber;
-		} else {
-			prevSibling.sibling = newFiber;
-		}
-		prevSibling = newFiber;
-	}
 	//* return next unit of work
 	//? 这里的思路比较好玩：首先检查child，如果没有child则检查sibling，如果还是没有就 [退回到 parent，检查sibling].loop
 	if (fiber.child) {
@@ -119,5 +109,72 @@ function performUnitOfWork(fiber: Fiber): Fiber {
 			f = f.parent;
 		}
 		return null;
+	}
+}
+
+const isSameType = (element: ElementNode | null, fiber: Fiber | null) =>
+	element && fiber && element.type === fiber.type;
+
+function reconcileChildren(children: ElementNode[], fiber: Fiber) {
+	let oldFiber = fiber.alternate ? fiber.alternate.child : null;
+	// oldFiber 对齐 fiber的第一个孩子，之后逐个对比即可
+	// fiber -> children: [child0, child1 ...]
+	//                       ↑ oldFiber
+
+	let prevSibling: Fiber = null;
+	let idx = 0;
+
+	while (idx < children.length || oldFiber) {
+		const child = children[idx];
+
+		//* 将child 和 oldFiber进行逐个对比
+		//* 只实现了效率非常低的 diff算法，没有考虑到位置变化的情况，而是直接对比
+		let newFiber: Fiber = null;
+
+		if (isSameType(child, oldFiber)) {
+			//* 相同type，更新
+			newFiber = {
+				type: oldFiber.type, //inherit
+				props: child.props, //update
+				dom: oldFiber.dom, //inherit
+				child: null, //reset
+				parent: fiber,
+				sibling: null,
+				alternate: oldFiber,
+				effectTag: "UPDATE", //flag
+			};
+		} else {
+			//* 只要type不相同，那么就卸载老节点，放入新节点
+			//! 完全没有优化
+			if (child) {
+				//oldFiber == null || oldFiber.type !== child.type
+				//* 添加新节点
+				newFiber = {
+					type: child.type,
+					props: child.props,
+					dom: null,
+					parent: fiber,
+					sibling: null,
+					child: null,
+					alternate: null,
+					effectTag: "PLACEMENT",
+				};
+			}
+			if (oldFiber) {
+				//chlid == null || oldFiber.type !== child.type
+				//* 删除老节点，这里采用标记收集，然后统一删除的处理策略
+				oldFiber.effectTag = "DELETION";
+				deletions.push(oldFiber);
+			}
+		}
+
+		//* 将 newFiber存储到对应位置
+		if (idx === 0) {
+			fiber.child = newFiber;
+		} else {
+			prevSibling.sibling = newFiber;
+		}
+		prevSibling = newFiber;
+		idx++;
 	}
 }
