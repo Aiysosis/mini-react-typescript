381af9715145ab9d617ee7794f82b62cdef900f6
Aiysosis <aiysosis@qq.com>
2022-12-18 15:12:18 +0800

    feat commitRoot

--- src/render-dom.ts
+++ src/render-dom.ts
@@ -49,10 +49,13 @@ type Fiber = {
 
 //* 全局变量：下一个要执行的 Fiber
 let nextUnitOfWork: Fiber = null;
+//* 全局变量 witRoot, 暂存没有渲染完的 根fiber,等全部渲染完成之后把整棵树一起挂到 root container上
+//* 这样用户就不会看到只渲染了一部分的界面
+let witRoot: Fiber = null;
 
 function render(element: ElementNode, container: HTMLElement | Text) {
 	//* 设置根部的 Fiber,它唯一的孩子节点是 element，父fiber和兄弟fiber都是空
-	nextUnitOfWork = {
+	const rootFiber: Fiber = {
 		type: "ROOT",
 		dom: container,
 		props: {
@@ -62,6 +65,7 @@ function render(element: ElementNode, container: HTMLElement | Text) {
 		sibling: null,
 		child: null,
 	};
+	nextUnitOfWork = witRoot = rootFiber;
 }
 
 function workLoop(deadline: IdleDeadline) {
@@ -70,10 +74,36 @@ function workLoop(deadline: IdleDeadline) {
 		nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
 		shouldYield = deadline.timeRemaining() < 1;
 	}
+	//* 如果全部节点都创建完毕，那么此时 witRoot == rootFiber, nextUnitOfWork == null;
+	//! 在更新时会不会导致重复挂载整棵树而带来性能问题？ -> 会，所以在commitRoot之后设置witRoot为null, 这样就解决了这个问题
+	if (!nextUnitOfWork && witRoot) {
+		commitRoot();
+	}
+
+	// 再次注册回调函数
 	requestIdleCallback(workLoop);
 }
 
+function commitRoot() {
+	//* 此时所有fiber内部的dom都已创建完成
+	commitWork(witRoot.child);
+	witRoot = null;
+}
+
+function commitWork(fiber: Fiber) {
+	//最开始拿到的是根节点的 child，也就是最外层元素对应的fiber
+	//? 从上往下挂载 or 从下往上挂载？
+	if (!fiber) return;
+	const parentDom = fiber.parent.dom;
+	parentDom.appendChild(fiber.dom);
+	commitWork(fiber.child);
+	commitWork(fiber.sibling);
+}
+
 //? 之前没有接触过的 api，它会在浏览器有空闲的时候执行我们的Fiber，并获取下一个 Fiber，直到执行完毕
+//? 可以理解为 setTimeout,只是这个延迟执行是由浏览器自动调度的，我们通过requestIdleCallback 注册回调之后
+//? 浏览器会在空闲的时候响应我们注册的请求
+
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber: Fiber): Fiber {
@@ -82,9 +112,10 @@ function performUnitOfWork(fiber: Fiber): Fiber {
 		fiber.dom = createDom(fiber);
 	}
 	//* 向父元素添加dom元素
-	if (fiber.parent) {
-		fiber.parent.dom.appendChild(fiber.dom);
-	}
+	//! 不直接提交，而是等全部节点创建完毕后统一提交 @ref -> witRoot
+	// if (fiber.parent) {
+	// 	fiber.parent.dom.appendChild(fiber.dom);
+	// }
 
 	//* create new fibers
 	const children = fiber.props.children;
